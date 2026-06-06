3bbb770f6eb1103caaed3722672f92057d051206
kingdomax <kingdomax@windowslive.com>
2026-06-06 21:01:41 +0700

    add live url mode

--- capacitor-app/capacitor.config.ts
+++ capacitor-app/capacitor.config.ts
@@ -1,9 +1,13 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.pramoch.agodahybridcapacitor',
-  appName: 'Agoda Hybrid Capacitor',
-  webDir: 'dist'
+    appId: "com.pramoch.agodahybridcapacitor",
+    appName: "Agoda Hybrid Capacitor",
+    webDir: "dist",
+    server: {
+        url: "http://10.0.2.2:5173",
+        cleartext: true,
+    },
 };
 
 export default config;
